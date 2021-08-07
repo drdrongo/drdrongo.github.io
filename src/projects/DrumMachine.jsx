51511c3f27cf732a53fc1fd9b36b1719cbec4098
hayato clarke <hayatoclarke@gmail.com>
2021-08-07 11:21:47 +0900

    save work

--- src/projects/DrumMachine.jsx
+++ src/projects/DrumMachine.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useEffect, useCallback, useState } from 'react';
+import React, { useMemo, useEffect, useCallback, useState, useRef } from 'react';
 import 'styles/projects/DrumMachine.scss';
 import boom from 'assets/sounds/DrumMachine/boom.wav';
 import clap from 'assets/sounds/DrumMachine/clap.wav';
@@ -9,8 +9,7 @@ import ride from 'assets/sounds/DrumMachine/ride.wav';
 import snare from 'assets/sounds/DrumMachine/snare.wav';
 import tink from 'assets/sounds/DrumMachine/tink.wav';
 import tom from 'assets/sounds/DrumMachine/tom.wav';
-
-let pressedKeys = [];
+import background from 'assets/images/view-from-stage.jpeg';
 
 const DrumMachine = () => {
     const drumSounds = useMemo(() => [
@@ -18,60 +17,109 @@ const DrumMachine = () => {
         { name: 'clap', sound: new Audio(clap), key: 's'},
         { name: 'hihat', sound: new Audio(hihat), key: 'd'},
         { name: 'kick', sound: new Audio(kick), key: 'f'},
-        { name: 'openhat', sound: new Audio(openhat), key: 'g'},
+        { name: 'open-hat', sound: new Audio(openhat), key: 'g'},
         { name: 'ride', sound: new Audio(ride), key: 'h'},
         { name: 'snare', sound: new Audio(snare), key: 'j'},
         { name: 'tink', sound: new Audio(tink), key: 'k'},
         { name: 'tom', sound: new Audio(tom), key: 'l'}
     ], []);
-
     
     const play = useCallback(audio => {
+        if (!audio) return;
+
         audio.pause();
         audio.currentTime = 0;
         audio.play();    
     }, []);
 
-    const [pressed, setPressed] = useState([]);
     useEffect(() => {
-        console.log('running useeffect');
         const keys = drumSounds.map(ds => ds.key);
         const playOnKeyDown = e => {
             const { sound } = drumSounds.find(ds => ds.key === e.key.toLowerCase());
             play(sound);
         };
+
         const onKeyDown = e => {
             if (e.repeat || !keys.includes(e.key.toLowerCase())) return;
+
             playOnKeyDown(e);
-            pressedKeys.push(e.key.toLowerCase());
-            setPressed(pressedKeys);
+            document.getElementById(`${e.key.toLowerCase()}-key`).classList.add('pressed');
         };
         const onKeyUp = e => {
-            pressedKeys = pressedKeys.filter(key => e.key.toLowerCase() !== key)
-            setPressed(pressedKeys);
-        }
+            if (e.repeat || !keys.includes(e.key.toLowerCase())) return;
+
+            document.getElementById(`${e.key.toLowerCase()}-key`).classList.remove('pressed');
+        };
+        
         document.addEventListener('keydown', onKeyDown);
         document.addEventListener('keyup', onKeyUp);
-        return () => document.removeEventListener('onkeydown', playOnKeyDown);
+
+        return () => {
+            document.removeEventListener('keydown', onKeyDown);
+            document.removeEventListener('keyup', onKeyUp);
+        };
     }, [drumSounds, play]);
 
-    const drumButtons = useMemo(() => (
-        drumSounds.map(({ name, sound, key }) => (
-            <button
-                className={`drum-button ${pressed.includes(key) ? 'pressed' : ''}`}
-                key={name}
-                onClick={() => {
-                    play(sound);
+
+    const drumContainer = useRef(null);
+    const [containerWidth, setContainerWidth] = useState(window.innerWidth * 0.9);
+
+    useEffect(() => {
+        window.addEventListener('resize', () => {
+            console.log("resizing!", drumContainer.current.offsetWidth)
+            setContainerWidth(drumContainer.current.offsetWidth * .9);
+        });
+    }, []);
+
+    const drumButtons = useMemo(() => {
+        const startAngle = (Math.PI / drumSounds.length),
+              radius = 260,
+              offset = containerWidth / 2;
+
+        let angle = startAngle + 5.8;
+        return drumSounds.map(({ name, sound, key }) => {
+            angle += startAngle;
+            return (
+                <button
+                    id={`${key}-key`}
+                    className={`drum-button`}
+                    style={{
+                        position: 'absolute',
+                        right: radius * Math.cos( angle ) + offset + "px",
+                        bottom: (radius * Math.sin( angle ) + 120) + "px"
+                    }}
+                    key={name}
+                    onMouseDown={e => {
+                        play(sound);
+                        e.currentTarget.classList.add('pressed');
+                    }}
+                    onMouseUp={e => e.currentTarget.classList.remove('pressed')}
+                    onMouseOut={e => e.currentTarget.classList.remove('pressed')}
+                >
+                    <span className="drum-key">{key}</span>
+                    <span className="drum-name">{name}</span>
+                </button>
+            );
+        }
+    )}, [containerWidth, drumSounds, play]);
+
+
+    return (
+        <div
+            className="DrumMachine-outer"
+            ref={drumContainer}
+            style={{ backgroundImage: `url(${background})` }}
+        >
+            <div
+                style={{
+                    position: 'relative',
+                    width: containerWidth,
+                    height: '100%',
+                    border: '1px solid red'
                 }}
             >
-                <span className="drum-key">{key}</span>
-                <span className="drum-name">{name}</span>
-            </button>
-        ))
-    ), [pressed, drumSounds, play]);
-    return (
-        <div className="DrumMachine-outer">
-            {drumButtons}
+                {drumButtons}
+            </div>
         </div>
     );
 };
