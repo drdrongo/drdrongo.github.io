51eb327d5bdeec69b41dae6e55541d232ab9b8a8
drdrongo <hayatoclarke@gmail.com>
2023-02-02 23:57:13 +0900

    made theme switch component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,38 @@
 import 'styles/components/Navbar.scss';
-import React from 'react';
-import {
-    Link,
-    useLocation
-} from "react-router-dom";
+import React, { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+
+const ThemeSwitch = () => {
+  const [isLight, setIsLight] = useState(true);
+
+  const switchTheme = () => setIsLight(!isLight);
+
+  return (
+    <button onClick={switchTheme} className={`ThemeSwitch ${isLight ? 'light' : 'dark'}`}>
+      <div />
+    </button>
+  );
+};
 
 const Navbar = ({ routes }) => {
-    const location = useLocation();
-    return (
-        <nav className="navbar">
-            <ul className="navbar__inner">
-            {routes.map(({ path, name, status }) => {
-                if (status === 404) return null;
+  const location = useLocation();
 
-                return (
-                    <li
-                        key={path}
-                        className={`navbar__item ${path === location.pathname ? 'on' : ''}`}
-                    >
-                        <Link to={path}>{name}</Link>
-                    </li>
-                )
-            })}
-            </ul>
-        </nav>
-    );
+  return (
+    <nav className="navbar">
+      <ThemeSwitch />
+      <ul className="navbar__inner">
+        {routes.map(({ path, name, status }) => {
+          if (status === 404) return null;
+
+          return (
+            <li key={path} className={`navbar__item ${path === location.pathname ? 'on' : ''}`}>
+              <Link to={path}>{name}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
